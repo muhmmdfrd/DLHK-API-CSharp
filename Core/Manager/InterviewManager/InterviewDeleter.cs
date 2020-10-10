9eee0fee7495cd3636db0fc4fde4b499f2a9a09e
muhmmdfrd <mfarid2121@gmail.com>
2020-10-10 20:09:09 +0700

    add ExcelController.cs and InterviewDeleter.cs

---
+++ Core/Manager/InterviewManager/InterviewDeleter.cs
@@ -0,0 +1,32 @@
+﻿using Repository;
+using System;
+using System.Linq;
+using System.Transactions;
+
+namespace Core.Manager.InterviewManager
+{
+	public class InterviewDeleter : AsistanceBase<InterviewAdapter, Interview>
+	{
+		public InterviewDeleter(InterviewAdapter manager) : base(manager)
+		{
+			// do nothing
+		}
+
+		public void Delete(long id)
+		{
+			using (var transac = new TransactionScope())
+			{
+				var data = Manager.Query.Value.Get().FirstOrDefault(x => x.InterviewId == id);
+
+				if (data == null)
+					throw new Exception("data not found");
+
+				Manager.Database.Interviews.Remove(data);
+
+				Manager.Database.SaveChanges();
+
+				transac.Complete();
+			}
+		}
+	}
+}
---
+++ DLHK-API/Controllers/ExcelController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Manager.EmployeeManager;
+using DLHK_API.Models;
+using System;
+using System.Web.Http;
+
+namespace DLHK_API.Controllers
+{
+	public class ExcelController : ApiController
+    {
+		private readonly ApiResponse<EmployeeDTO> resp = new ApiResponse<EmployeeDTO>();
+        
+        [HttpPost]
+        public IHttpActionResult ImportFromExcel()
+        {
+            try
+			{
+				new Utilities.Util().ExcelUpload();
+
+				resp.Message = "data uploaded";
+				resp.MessageCode = 201;
+				resp.ErrorCode = 0;
+				resp.Data = null;
+			}
+            catch (Exception ex)
+			{
+				resp.Message = ex.Message;
+				resp.MessageCode = 500;
+				resp.ErrorCode = 1;
+				resp.Data = null;
+			}
+
+			return Json(resp);
+		}
+    }
+}
